92e52bb58e201b5877ec854bc71bf05c0b32e7ef
bluefeufeu <168778203+bleufeufeu@users.noreply.github.com>
2024-07-03 20:39:31 -0400

    add solution

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,10 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+    if (num < 0) return "ERROR";
+    let value = "";
+    for (i = 0; i < 3; i++) {
+        value += string;
+    }
+    return value
 };
 
 // Do not edit below this line
