b587a1fe16125545aa367d396f0de02fc24343f3
bluefeufeu <168778203+bleufeufeu@users.noreply.github.com>
2024-07-03 22:24:11 -0400

    add solution

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,21 @@
 const sumAll = function() {
-
+    let sum = 0;
+    if (arguments[1] > arguments[0]) {
+        left = arguments[0];
+        right = arguments[1];        
+    } else {
+        left = arguments[1];
+        right = arguments[0];
+    }
+  
+    if (Number.isInteger(left) && Number.isInteger(right) && left > 0 && right > 0) {
+        for (i = left; i <= right; i++) {
+            sum += i;
+        }
+        return sum;
+    } else {
+        return 'ERROR';
+    }
 };
 
 // Do not edit below this line
