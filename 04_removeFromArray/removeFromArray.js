aaf23f5d45191e875497d828539b4ef6277f547d
bluefeufeu <168778203+bleufeufeu@users.noreply.github.com>
2024-07-03 21:48:07 -0400

    add solution

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,8 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...indices) {
+    for (i = 0; i < indices.length; i++) {
+        array = array.filter((array) => array !== indices[i]);
+    }
+    return array;
 };
 
 // Do not edit below this line
