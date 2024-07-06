bfe0e3f84f3a96f9b2ed204d4c32d7fa49c384c7
bluefeufeu <168778203+bleufeufeu@users.noreply.github.com>
2024-07-06 04:12:49 -0400

    add solution

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,16 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+    let maxAge = 0;
+    let eldest;
+    for (const person of people) {
+        let age = 0;
+        if (!person.yearOfDeath) age = new Date().getFullYear() - person.yearOfBirth;
+        else age = person.yearOfDeath - person.yearOfBirth;
+        if (age > maxAge) {
+            maxAge = age;
+            eldest = person;
+        }
+    }
+    return eldest;
 };
 
 // Do not edit below this line
