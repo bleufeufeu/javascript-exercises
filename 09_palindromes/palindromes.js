515e263b6ab28a1c3f74f053de6f78dee4338cb1
bluefeufeu <168778203+bleufeufeu@users.noreply.github.com>
2024-07-06 03:10:23 -0400

    add solution

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,12 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+    string = string.replace(/[,.?! ]/g, "").toLowerCase();
+    let newString = "";
+    for (let i = 0; i < string.length; i++) {
+        //console.log(string[string.length - i - 1].match(/[A-Za-z]/g) !== null);
+            newString += string[string.length - i - 1].toLowerCase();
+        
+    }
+    return string == newString;
 };
 
 // Do not edit below this line
