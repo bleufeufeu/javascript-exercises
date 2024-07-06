e5cbf4be4452910384e2fb41f860655ebd546829
bluefeufeu <168778203+bleufeufeu@users.noreply.github.com>
2024-07-06 02:37:54 -0400

    add solution

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,38 @@
 const add = function() {
-	
+  const args = Array.from(arguments);
+	let sum = args.reduce((total, currentValue) => {
+    return total + currentValue;
+  }, 0);
+  return sum;
 };
 
 const subtract = function() {
-	
+	const args = Array.from(arguments);
+	let sum = args.reduce((total, currentValue) => {
+    return total - currentValue;
+  });
+  return sum;
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+	return array.reduce((total, current) => total + current, 0);
 };
 
-const multiply = function() {
-
+const multiply = function(array) {
+	return array.reduce((total, current) => total * current);
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+	return a ** b;
 };
 
-const factorial = function() {
-	
+const factorial = function(n) {
+  if (n === 0) return 1;
+  prod = 1;
+	for (let i = n; i > 0; i--) {
+    prod *= i 
+  }
+  return prod
 };
 
 // Do not edit below this line
