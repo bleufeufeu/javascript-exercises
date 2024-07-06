a25f8bee82ddac667ceb49e0f039fe3dc139a1be
bluefeufeu <168778203+bleufeufeu@users.noreply.github.com>
2024-07-06 03:34:36 -0400

    add solution

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,12 @@
-const fibonacci = function() {
-
+const fibonacci = function(num) {
+    num = Number(num);
+    if (num === 0) return 0;
+    if (num < 0) return "OOPS";
+    let fibonacciSeq = [1, 1];    
+    for (let i = 2; i < num; i++) {
+        fibonacciSeq[i] = fibonacciSeq[i-1] + fibonacciSeq[i-2];
+    }
+    return fibonacciSeq[num-1];
 };
 
 // Do not edit below this line
