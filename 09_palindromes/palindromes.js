199f53d61649813848305be6d21d0e5d138b0fcd
bluefeufeu <168778203+bleufeufeu@users.noreply.github.com>
2024-07-06 03:13:29 -0400

    changed '==' to '==='

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -3,10 +3,9 @@ const palindromes = function (string) {
     let newString = "";
     for (let i = 0; i < string.length; i++) {
         //console.log(string[string.length - i - 1].match(/[A-Za-z]/g) !== null);
-            newString += string[string.length - i - 1].toLowerCase();
-        
+            newString += string[string.length - i - 1].toLowerCase(); 
     }
-    return string == newString;
+    return string === newString;
 };
 
 // Do not edit below this line
